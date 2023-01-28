a9767c2a78efa58600a70991a434438ac4caca20
Tim Graf <tim.graf@gmail.com>
2023-01-28 16:13:25 +0100

    set build default for lily58 to promicro_rp2040

--- keyboards/splitkb/aurora/lily58/rules.mk
+++ keyboards/splitkb/aurora/lily58/rules.mk
@@ -14,3 +14,5 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 DEFAULT_FOLDER = splitkb/aurora/lily58/rev1
+# Convert from Elite-C to standard RP2040
+CONVERT_TO=promicro_rp2040
